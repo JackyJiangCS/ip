0f94c29894426e74450493c9aaa661affad9beb9
Jacky Jiang RongTang <e1459669@u.nus.edu>
2026-08-21 21:45:34 +0800

    Add greeting and exit messages

--- src/main/java/JassaBot.java
+++ src/main/java/JassaBot.java
@@ -3,11 +3,19 @@
  */
 public class JassaBot {
     public static void main(String[] args) {
+        String divider = "____________________________________________________________";
         String banner = "   _                         ____        _\n"
                 + "  | | __ _ ___ ___  __ _    | __ )  ___ | |_\n"
                 + "  | |/ _` / __/ __|/ _` |   |  _ \\ / _ \\| __|\n"
                 + "  | | (_| \\__ \\__ \\ (_| |   | |_) | (_) | |_\n"
                 + " _|_|\\__,_|___/___/\\__,_|   |____/ \\___/ \\__|";
+
+        System.out.println(divider);
         System.out.println(banner);
+        System.out.println("Hello! I'm JassaBot.");
+        System.out.println("What can I do for you?");
+        System.out.println(divider);
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println(divider);
     }
 }
