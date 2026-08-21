12fff861a055da4cc5a45a2038bdd6fea1091103
Jacky Jiang RongTang <e1459669@u.nus.edu>
2026-08-21 21:04:36 +0800

    Rename chatbot to JassaBot and update ASCII banner

---
+++ src/main/java/JassaBot.java
@@ -0,0 +1,13 @@
+/**
+ * Starts the JassaBot chatbot application.
+ */
+public class JassaBot {
+    public static void main(String[] args) {
+        String banner = "   _                         ____        _\n"
+                + "  | | __ _ ___ ___  __ _    | __ )  ___ | |_\n"
+                + "  | |/ _` / __/ __|/ _` |   |  _ \\ / _ \\| __|\n"
+                + "  | | (_| \\__ \\__ \\ (_| |   | |_) | (_) | |_\n"
+                + " _|_|\\__,_|___/___/\\__,_|   |____/ \\___/ \\__|";
+        System.out.println(banner);
+    }
+}
